2e6898b0f47652ce3a5dfc1f65da8c9c2d9ac6e1
BMAsekunowo <bm.asekunowo@gmail.com>
2025-02-26 10:05:02 +0100

    udated file structure & second page first draft

---
+++ public/script.js
@@ -0,0 +1,19 @@
+function toggleForm() {
+    let container = document.getElementById('container');
+    let signInForm = document.getElementById('sign-in');
+    let signUpForm = document.getElementById('sign-up');
+    let animePanel = document.getElementById('animation-panel');
+    let formPanel = document.getElementById('form-panel');
+
+    if (signUpForm.style.display === "none" || signUpForm.style.display === "") {
+        signInForm.style.display = "none";
+        signUpForm.style.display = "block";
+        animePanel.style.transform = "translateX(-100%)";  // ← This makes the panels exchange positions.
+        formPanel.style.transform = "translateX(100%)";
+    } else {
+        signInForm.style.display = "block";
+        signUpForm.style.display = "none";
+        animePanel.style.transform = "translateX(0%)"; // ← This basically leaves the panels in their original position.
+        formPanel.style.transform = "translateX(0%)";
+    }
+}
