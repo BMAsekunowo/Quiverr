b18447adb1e27a0a1e790878e275618eb8a6555c
BMAsekunowo <bm.asekunowo@gmail.com>
2025-03-01 00:46:07 +0100

    commit --Here's where I stop today

--- public/homepage.js
+++ public/homepage.js
@@ -47,3 +47,34 @@ if (welcomeUser && userImg) {
     } else {
         console.error("Error: Missing required elements!");
     }
+
+
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = 'BQAD1AOxOh_JgERrENtJHSaGoLRiLfWqAtSkqvfLSBMBSnryPuNYqaW_ev6U1z4ozHi_eFzVxy91JY9CXldBuUZ5F3HbposhazbVRVSFvDWYjr2bZQJYuZPk1g6iwYkGkrlZZQmL8zCqoGUdibo6HiNRxTjEY8THFru881gL1IfTuFSd4I50hTDVtYWXNAH2g6krTY8vdLjhSAEIDsbCdGcI9sF7m8d5dkZNgFhi4DP5z0bjdQPbIrnzMgD6wgJ3Qd3zFuFR4-ljUsEoLTypVr6Xg2AHscFKnJCGWRTc2UeTyantPYTvB77USec3';
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+  )).items;
+}
+
+const topTracks = await getTopTracks();
+console.log(
+  topTracks?.map(
+    ({name, artists}) =>
+      `${name} by ${artists.map(artist => artist.name).join(', ')}`
+  )
+);
+
+window.fetchWebApi = fetchWebApi;
